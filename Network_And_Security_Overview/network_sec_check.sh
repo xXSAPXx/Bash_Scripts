72e2bdd55ad28140fa85141287f44272c40d023b
xXSAPXx <113801124+xXSAPXx@users.noreply.github.com>
2024-12-26 21:35:48 +0200

    Update network_sec_check.sh

--- Network_And_Security_Overview/network_sec_check.sh
+++ Network_And_Security_Overview/network_sec_check.sh
@@ -1,36 +1,54 @@
 #!/bin/bash
 set -euo pipefail
 
+# Variables: 
+GREEN="\e[32m"
+RED="\e[31m"
+YELLOW="\e[33m"
+BLUE='\033[1;34m'
+RESET="\e[0m"
+
 # Inform the user that the script is running:
-echo "Loading Information. Please wait..."
+echo
+echo -e "╰┈➤   ${YELLOW}Loading Information. Please wait... 🔧${RESET}"
+echo
 
 # Test connectivity to a specific host (example Google's DNS: 8.8.8.8)
 ping -c 4 8.8.8.8 > /dev/null
 CONNECTION_STATUS=$?
 
 # Display summary of network status
-echo -e "\nNetwork and Security Information: $(date '+%Y-%m-%d %H:%M:%S')"
-echo "- Connectivity: $(if [[ $CONNECTION_STATUS -eq 0 ]]; then echo "Successful"; else echo "Failed"; fi)"
-echo "- Interfaces: $(ip link | grep UP | awk '{print $2}' | paste -sd ', ' -)"
-echo "- IP Addresses: $(ip addr show | awk '/inet/ {print $2}' | cut -d/ -f1 | paste -sd ', ' -)"
-echo "- Default Gateway: $(ip route | awk '/default/ {print $3}')"
-echo "- External IP Address: $(curl -s ifconfig.me)"
-echo "- Public DNS Servers: $(nslookup -type=NS google.com | awk '/Server:/ {print $2}')"
-
-# Network and Security Information
-echo "- Firewall Status: $(systemctl is-active firewalld)"
-echo "- Active Users: $(who)"
-echo "- Open Ports: $(netstat -tunlp | awk '/LISTEN/ {gsub(/.*:/, "", $4); print $4}' | tr '\n' ', ')"
-echo "- OpenSSH Version: $(ssh -V 2>&1 | cut -d' ' -f1-2)"
-echo "- Heartbleed vulnerability (OpenSSL): $(openssl version -a | grep -q 'OpenSSL 1.0.1[0-9a-f]*' && echo "Vulnerable, Update OpenSSL!" || echo "Not Vulnerable")"
-echo "- Shellshock vulnerability (Bash): $(env 'VAR=() { :;}; echo vulnerable' 'FUNCTION()=() { :;}; echo Vulnerable' bash -c 'echo Not Vulnerable')"
+echo "==================================================="
+echo -e "System Network Status: [${BLUE}$(date '+%Y-%m-%d %H:%M:%S')${RESET}]"
 echo
-echo "- Listening Services:"
+echo -e "- Connectivity: $(if [[ $CONNECTION_STATUS -eq 0 ]]; then echo "${GREEN}Successful${RESET}"; else echo "${RED}Failed${RESET}"; fi)"
+echo "- Interfaces: $(ip link | grep UP | awk '{print $2}' | paste -sd ' , ' -)"
+echo "- Internal IP Address: $(ip -4 addr show | awk '/inet / && $2 !~ /^127/ {print $2}' | paste -sd ', ' -)"
+echo "- External IP Address: $(curl -s ifconfig.me)"
+echo "- Default Gateway: $(ip route | awk '/default/ {print $3}')"
+echo "- Public DNS Servers: $(nslookup -type=NS google.com | awk '/Server:/ {print $2}')"
+echo
+
+# Display Network and Security Information
+echo "==================================================="
+echo "Network and Security Information:"
+echo
+echo -e "- Firewall Status (firewalld): $(command -v systemctl &>/dev/null && systemctl is-active firewalld || echo -e "${YELLOW}Not Installed (Security Groups likely used)${RESET}")"
+echo -e "- Active Users: $(who)"
+echo -e "- Open Ports: $(ss -tunlp | awk '/LISTEN/ && $5 ~ /:/ {split($5, a, ":"); print a[2]}' | sort -n | uniq | awk 'NF {printf "[%s] ", $0}')"
+echo -e "- OpenSSH Version: $(ssh -V 2>&1 | cut -d' ' -f1-2)"
+echo -e "- Heartbleed vulnerability (OpenSSL): $(openssl version -a | grep -q 'OpenSSL 1.0.1[0-9a-f]*' && echo "Vulnerable, Update OpenSSL!" || echo "Not Vulnerable")"
+echo -e "- Shellshock vulnerability (Bash): $(env 'VAR=() { :;}; echo vulnerable' 'FUNCTION()=() { :;}; echo Vulnerable' bash -c 'echo Not Vulnerable')"
+echo
+echo "==================================================="
+echo "Listening Services:"
+echo
 echo "$(ss -tunlp | awk '/LISTEN/ {print "Service:", $7, "| Protocol:", $1, "| User:", $NF}' | column -t)"
 echo
 
-echo  "- ARP Table :"
-echo "$(arp -a)"
+# Display ARP Table: 
+echo "- ARP Table :"
+echo "$(ip neigh)"
 echo
 echo "- Active Connections:"
-echo "$(netstat -tunapl | grep ESTABLISHED)" 
+echo "$(ss -tunapl | grep ESTABLISHED)" 
