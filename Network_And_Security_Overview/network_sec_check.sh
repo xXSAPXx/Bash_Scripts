b48a71df310f8b83490022f89b2450f6571c6618
xXSAPXx <113801124+xXSAPXx@users.noreply.github.com>
2024-12-29 16:20:06 +0200

    Added new features for ports and SELINUX

--- Network_And_Security_Overview/network_sec_check.sh
+++ Network_And_Security_Overview/network_sec_check.sh
@@ -10,10 +10,12 @@ RED="\e[31m"
 YELLOW="\e[33m"
 RESET="\e[0m"
 
+
 # Inform the user that the script is running:
 echo
-echo -e "╰┈➤   ${YELLOW}Loading Information. Please wait... 🔧${RESET}"
+echo -e "╰┈➤   ${YELLOW}Loading Information. Please wait... 🔧${RESET} - [${YELLOW}$(date '+%Y-%m-%d %H:%M:%S')${RESET}]"
 echo
+
 
 # Test connectivity to a specific host (Google's DNS: 8.8.8.8)
 ping -c 4 8.8.8.8 > /dev/null
@@ -23,10 +25,14 @@ CONNECTION_STATUS=$?
 command -v systemctl &>/dev/null && systemctl is-active firewalld > /dev/null
 FIREWALL_STATUS=$?
 
+# Test SELinux Status: 
+SELINUX_STATUS=$(getenforce)
 
-# Display summary of network status
+
+
+# Display summary of network status:
 echo "==================================================="
-echo -e "System Network Status: [${LBLUE}$(date '+%Y-%m-%d %H:%M:%S')${RESET}]"
+echo "System Network Status:"
 echo
 echo -e "- Connectivity: $(if [[ $CONNECTION_STATUS -eq 0 ]]; then echo "${GREEN}Successful${RESET}"; else echo "${RED}Failed${RESET}"; fi)"
 echo "- Interfaces: $(ip link | grep UP | awk '{print $2}' | paste -sd ' , ' -)"
@@ -36,18 +42,19 @@ echo "- Default Gateway:     $(ip route | awk '/default/ {print $3}')"
 echo "- Public DNS Servers:  $(nslookup -type=NS google.com | awk '/Server:/ {print $2}')"
 echo
 
-# Display Network and Security Information
+# Display Network and Security Information:
 echo "==================================================="
 echo "Network and Security Information:"
 echo
+echo -e "- SELinux Status: $(if [[ $SELINUX_STATUS == "Enforcing" ]]; then echo "${GREEN}Enforcing${RESET}"; elif [[ $SELINUX_STATUS == "Permissive" ]]; then echo "${YELLOW}Permissive${RESET}"; else echo "${RED}Disabled${RESET}"; fi)"
 echo -e "- Firewall Status (firewalld): $(if [[ $FIREWALL_STATUS -eq 0 ]]; then echo "${GREEN}Active${RESET}"; else echo "${RED}Inactive${RESET} --- ${YELLOW}(Security Groups Likely Used)${RESET}"; fi)"
 echo -e "- Open Ports: $(if [[ $FIREWALL_STATUS -eq 0 ]]; then echo -e $(firewall-cmd --list-ports | tr ' ' '\n' | awk '{printf "[%s]  ", $1}'); else echo -e "${RED}N/A${RESET}"; fi)"
 echo -e "- OpenSSH Version: $(ssh -V 2>&1 | cut -d' ' -f1-2)"
-echo -e "- Heartbleed vulnerability (OpenSSL): $(openssl version -a | grep -q 'OpenSSL 1.0.1[0-9a-f]*' && echo "Vulnerable, Update OpenSSL!" || echo "Not Vulnerable")"
-echo -e "- Shellshock vulnerability (Bash): $(env 'VAR=() { :;}; echo vulnerable' 'FUNCTION()=() { :;}; echo Vulnerable' bash -c 'echo Not Vulnerable')"
 echo
+
+# Display Listening Services and Ports: 
 echo "==================================================="
-echo "Listening Services:"
+echo -e "Listening Services: State [${LBLUE}LISTENING${RESET}]"
 echo
 echo -e "- Service Ports in Use: $(ss -tunlp | awk '/LISTEN/ && $5 ~ /:/ {split($5, a, ":"); print a[2]}' | sort -n | uniq | awk 'NF {printf "[%s] ", $0}')"
 echo  
@@ -58,17 +65,28 @@ echo -e "$(ss -tunlp | awk '/LISTEN/ {
     cmd = "ps -o user= -p " pid;
     cmd | getline user;
     close(cmd);
-    print "  Service:", $7, "|| Port:", $5, "|| Protocol:", $1, "|| User:", user }' | column -t)"
+    print "  Service:", $7, "|| Port:", substr($5, match($5, /[^:]*$/)), "|| Protocol:", $1, "|| User:", user }' | column -t)"
     
 echo 
-echo
- 
 
-#echo "$(ss -tunlp | awk '/LISTEN/ {print "${LGREEN}Service:${RESET}", $7, "|| ${LBLUE}Protocol:${RESET}", $1, "|| ${YELLOW}User:${RESET}", $NF}' | column -t)"
+# Display Established Services and Ports: 
+echo "==================================================="
+echo -e "Active Connections: State [${LBLUE}ESTABLISHED${RESET}]"
+echo
+echo -e "$(ss -tunlpa | awk '/ESTAB / { 
+    split($7, service_info, "=");
+    pid = service_info[2];
+    sub(/[),].*/, "", pid);
+    cmd = "ps -o user= -p " pid;
+    cmd | getline user;
+    close(cmd);
+    print "  Service:", $7, "|| Port:", substr($5, match($5, /[^:]*$/)), "|| Protocol:", $1, "|| User:", user }' | column -t)"
+
+echo
+echo
+
+
 # Display ARP Table: 
 # echo "- ARP Table :"
 # echo "$(ip neigh)"
-# echo
-# echo "- Active Connections:"
-# echo "$(ss -tunapl | grep ESTABLISHED)" 
-# echo -e "- Active Users: $(who)"
+
